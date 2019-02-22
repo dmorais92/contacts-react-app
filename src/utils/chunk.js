3cc2159463d187d9ad525570c0f09b6bad07b923
David Morais <davidmorais92@gmail.com>
2019-02-22 00:30:52 +0000

    Add chunk utils

---
+++ src/utils/chunk.js
@@ -0,0 +1,21 @@
+export default function chunk(array, chunks) {
+  const result = [];
+  if (array && array.length) {
+    let arrayItterator = 0;
+    while (arrayItterator < array.length) {
+      let chunk = [];
+      let i = 0;
+      while (i < chunks) {
+        if (array.indexOf(array[arrayItterator + i]) !== -1) {
+          chunk.push(array[arrayItterator + i]);
+        }
+        i ++;
+      }
+      if (chunk.length) {
+        result.push(chunk);
+      }
+      arrayItterator ++;
+    }
+  }
+  return result;
+}
