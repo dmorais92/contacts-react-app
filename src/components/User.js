14a822036013b45cf9c784445564e6a59bfe139f
David Morais <davidmorais92@gmail.com>
2019-02-22 18:54:24 +0000

    Render user address   - Add functional component to render geographic fields   - Clicking on a geo field will open a new tab in google maps with the given coordinates

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,27 @@ import logo from "../logo.svg";
 import Card from "./common/Card";
 
 import "./User.scss";
-import chunk from "../utils/chunk";
+import { chunk, capitalize } from "../utils";
+
+const renderGeoFields = coords => {
+  const { lat, lng } = coords;
+  return (
+    (lat && lng && (
+      <div className="geo-fields title-value" key="geo">
+        <a
+          target="blank"
+          href={`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`}
+        >
+          <h5>Location</h5>
+          <span>
+            {lat}º, {lng}º
+          </span>
+        </a>
+      </div>
+    )) ||
+    ""
+  );
+};
 
 class UserList extends PureComponent {
   static propTypes = {
@@ -19,46 +39,53 @@ class UserList extends PureComponent {
     })
   };
 
-  renderUserFields(fields) {
-    const { user } = this.props;
+  renderUserFields(fields, target = this.props.user) {
     if (fields && fields.length) {
-      return(
-        chunk(fields, 3).map(section => (
-          <div className="info-section" key={`${fields}-${section}`}>
-            {section.map(field =>
-              (
-                <div className="title-value" key={`${field}-${user[field]}`}>
-                  <h5>{field}</h5>
-                  <span>{user[field]}</span>
-                </div>
-              )
-            )}
-          </div>
-        ))
-      )
-
+      return chunk(fields, 3).map(section => (
+        <div className="info-section" key={`${fields}-${section}`}>
+          {section.map(field => {
+            switch (field) {
+              case "geo":
+                return renderGeoFields(target[field]);
+              default:
+                return (
+                  <div
+                    className="title-value"
+                    key={`${field}-${target[field]}`}
+                  >
+                    <h5>{capitalize(field)}</h5>
+                    <span>{target[field]}</span>
+                  </div>
+                );
+            }
+          })}
+        </div>
+      ));
     }
   }
 
   render() {
     const { user } = this.props;
-    const { name, id, username, email, address, company, ...userInfo } = user;
-    const extraFields = userInfo && Object.keys(userInfo);
+    if (user) {
+      const { name, id, username, email, address, company, ...userInfo } = user;
+      const extraFields = userInfo && Object.keys(userInfo);
 
-
-    return (
-      <Card vertical key={id} classes={["User"]}>
-        <div className="row">
-          <img src={logo} className="logo" alt="logo" />
-          <div className="main-info">
-            <h3>{name}</h3>
-            <span>{email}</span>
-            <span>{username}</span>
+      return (
+        <Card vertical key={id} classes={["User"]}>
+          <div className="row">
+            <img src={logo} className="logo" alt="logo" />
+            <div className="main-info">
+              <h3>{name}</h3>
+              <span>{email}</span>
+              <span>{username}</span>
+            </div>
           </div>
-        </div>
-        {this.renderUserFields(extraFields)}
-      </Card>
-    );
+          {this.renderUserFields(extraFields)}
+          {this.renderUserFields(Object.keys(address), address)}
+        </Card>
+      );
+    }
+    return '';
   }
 }
 
