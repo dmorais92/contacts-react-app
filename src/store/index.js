95573fd867d468f17f376883b2868db02da7a69e
David Morais <davidmorais92@gmail.com>
2019-02-23 23:40:08 +0000

    Add redux-thunk to use bind two actions together

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import RootReducer from './rootReducer';
 
 const store = createStore(RootReducer, {
   users: {
     userList: [],
   }
-}, applyMiddleware(logger));
+}, applyMiddleware(logger, thunk));
 
 
 export default store;
