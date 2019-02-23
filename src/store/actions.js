5f79ecf9ffad8796127a5851f00b93909198f939
David Morais <davidmorais92@gmail.com>
2019-02-23 14:18:10 +0000

    Add Redux   - Create store   - Add redux-logger and apply middleware   - Add actions

---
+++ src/store/actions.js
@@ -0,0 +1,21 @@
+const ACTION_TYPES = {
+  USERS: {
+    GET_USERS: 'GET_USERS',
+    GET_USERS_FAILED: 'GET_USERS_FAILED',
+    GET_USERS_SUCCESS: 'GET_USERS_SUCCESS',
+  }
+};
+
+export const getUsers = () => ({
+  type: ACTION_TYPES.USERS.GET_USERS,
+});
+
+export const getUsersSucess = users => ({
+  type: ACTION_TYPES.USERS.GET_USERS_SUCCESS,
+  users,
+});
+
+export const getUsersFaield = (e) => ({
+  type: ACTION_TYPES.USERS.GET_USERS_FAILED,
+  error: e
+})
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import RootReducer from './rootReducer';
+
+const store = createStore(RootReducer, applyMiddleware(logger));
+
+
+export default store;
